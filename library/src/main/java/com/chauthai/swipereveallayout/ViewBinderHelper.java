60aeb3defb1d0553a6499c6ef1c25dcea18b45bc
chthai64 <chthai64@gmail.com>
2016-04-12 17:27:12 -0400

    Able to save/restore when the device orientation is changed

--- library/src/main/java/com/chauthai/swipereveallayout/ViewBinderHelper.java
+++ library/src/main/java/com/chauthai/swipereveallayout/ViewBinderHelper.java
@@ -1,18 +1,29 @@
 package com.chauthai.swipereveallayout;
 
+import android.os.Bundle;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Chau Thai on 4/11/16.
  */
 public class ViewBinderHelper {
-    private final Map<String, Integer> mapStates = new HashMap<>();
+    private static final String BUNDLE_MAP_KEY = "ViewBinderHelper_Bundle_Map_Key";
+    private HashMap<String, Integer> mapStates = new HashMap<>();
 
-    public void bind(final SwipeRevealLayout paneLayout, final String id) {
-        paneLayout.abort();
+    /**
+     * Help to save and restore open/close state of the swipeLayout. Call this method
+     * when you bind your view holder with the data object.
+     *
+     * @param swipeLayout swipeLayout of the current view.
+     * @param id a string that uniquely defines the data object of the current view.
+     */
+    public void bind(final SwipeRevealLayout swipeLayout, final String id) {
+        swipeLayout.abort();
 
-        paneLayout.setDragStateChangeListener(new SwipeRevealLayout.DragStateChangeListener() {
+        swipeLayout.setDragStateChangeListener(new SwipeRevealLayout.DragStateChangeListener() {
             @Override
             public void onDragStateChanged(int state) {
                 mapStates.put(id, state);
@@ -21,18 +32,58 @@ public class ViewBinderHelper {
 
         if (!mapStates.containsKey(id)) {
             mapStates.put(id, SwipeRevealLayout.STATE_CLOSE);
-            paneLayout.close(false);
+            swipeLayout.close(false);
         } else {
             int state = mapStates.get(id);
 
             if (state == SwipeRevealLayout.STATE_CLOSE || state == SwipeRevealLayout.STATE_CLOSING ||
                     state == SwipeRevealLayout.STATE_DRAGGING) {
-                paneLayout.close(false);
+                swipeLayout.close(false);
             } else {
-                paneLayout.open(false);
+                swipeLayout.open(false);
+            }
+        }
+    }
+
+    /**
+     * Only if you need to restore open/close state when the orientation is changed.
+     * Call this method in {@link android.app.Activity#onSaveInstanceState(Bundle)}
+     */
+    public void saveStates(Bundle outState) {
+        if (outState == null)
+            return;
+
+        Bundle statesBundle = new Bundle();
+        for (Map.Entry<String, Integer> entry : mapStates.entrySet()) {
+            statesBundle.putInt(entry.getKey(), entry.getValue());
+        }
+
+        outState.putBundle(BUNDLE_MAP_KEY, statesBundle);
+    }
+
+
+    /**
+     * Only if you need to restore open/close state when the orientation is changed.
+     * Call this method in {@link android.app.Activity#onRestoreInstanceState(Bundle)}
+     */
+    @SuppressWarnings({"unchecked", "ConstantConditions"})
+    public void restoreStates(Bundle inState) {
+        if (inState == null)
+            return;
+
+        if (inState.containsKey(BUNDLE_MAP_KEY)) {
+            HashMap<String, Integer> restoredMap = new HashMap<>();
+
+            Bundle statesBundle = inState.getBundle(BUNDLE_MAP_KEY);
+            Set<String> keySet = statesBundle.keySet();
+
+            if (keySet != null) {
+                for (String key : keySet) {
+                    restoredMap.put(key, statesBundle.getInt(key));
+                }
             }
 
-
+            mapStates = restoredMap;
         }
     }
 }
