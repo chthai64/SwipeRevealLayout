7918e14cb8975fdb5b5606b38cdf7e03508a792d
bircumaxim <maxim.bircu@jivygroup.com>
2016-12-07 11:23:15 +0200

    Implemented start and end flags for Android Native RTL support.

--- swipe-reveal-layout/src/main/java/com/chauthai/swipereveallayout/SwipeRevealLayout.java
+++ swipe-reveal-layout/src/main/java/com/chauthai/swipereveallayout/SwipeRevealLayout.java
@@ -41,6 +41,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import static android.content.res.Configuration.SCREENLAYOUT_LAYOUTDIR_MASK;
+import static android.content.res.Configuration.SCREENLAYOUT_LAYOUTDIR_RTL;
+
 @SuppressLint("RtlHardcoded")
 public class SwipeRevealLayout extends ViewGroup {
     // These states are used only for ViewBindHelper
@@ -57,6 +60,8 @@ public class SwipeRevealLayout extends ViewGroup {
     public static final int DRAG_EDGE_RIGHT =  0x1 << 1;
     public static final int DRAG_EDGE_TOP =    0x1 << 2;
     public static final int DRAG_EDGE_BOTTOM = 0x1 << 3;
+    public static final int DRAG_EDGE_START = 0x1 << 4;
+    public static final int DRAG_EDGE_END = 0x1 << 5;
 
     /**
      * The secondary view will be under the main view.
@@ -518,7 +523,17 @@ public class SwipeRevealLayout extends ViewGroup {
      *                 </ul>
      */
     public void setDragEdge(int dragEdge) {
-        mDragEdge = dragEdge;
+        switch (dragEdge){
+            case DRAG_EDGE_START:
+                mDragEdge = isRTL() ? DRAG_EDGE_RIGHT : DRAG_EDGE_LEFT;
+                break;
+            case DRAG_EDGE_END:
+                mDragEdge = isRTL() ? DRAG_EDGE_LEFT : DRAG_EDGE_RIGHT;
+                break;
+            default:
+                mDragEdge = dragEdge;
+                break;
+        }
     }
 
     /**
@@ -692,7 +707,7 @@ public class SwipeRevealLayout extends ViewGroup {
                     0, 0
             );
 
-            mDragEdge = a.getInteger(R.styleable.SwipeRevealLayout_dragEdge, DRAG_EDGE_LEFT);
+            setDragEdge(a.getInteger(R.styleable.SwipeRevealLayout_dragEdge, DRAG_EDGE_LEFT));
             mMinFlingVelocity = a.getInteger(R.styleable.SwipeRevealLayout_flingVelocity, DEFAULT_MIN_FLING_VELOCITY);
             mMode = a.getInteger(R.styleable.SwipeRevealLayout_mode, MODE_NORMAL);
 
@@ -1067,5 +1082,9 @@ public class SwipeRevealLayout extends ViewGroup {
         Resources resources = getContext().getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
         return (int) (dp * ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    private boolean isRTL(){
+        return (getResources().getConfiguration().screenLayout & SCREENLAYOUT_LAYOUTDIR_MASK) == SCREENLAYOUT_LAYOUTDIR_RTL;
     }
 }
